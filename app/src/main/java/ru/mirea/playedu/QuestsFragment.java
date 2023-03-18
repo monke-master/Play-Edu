7886ee7365bf9e2b906c42236d03a2b70129a29e
Игорь <igor.streshnev2019@gmail.com>
2023-03-18 12:12:13 +0300

    added power spinner

--- app/src/main/java/ru/mirea/playedu/QuestsFragment.java
+++ app/src/main/java/ru/mirea/playedu/QuestsFragment.java
@@ -2,6 +2,7 @@ package ru.mirea.playedu;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,50 +12,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.skydoves.powerspinner.OnSpinnerItemSelectedListener;
+import com.skydoves.powerspinner.PowerSpinnerView;
+
 import ru.mirea.playedu.databinding.FragmentQuestsBinding;
-
-
 
 public class QuestsFragment extends Fragment {
 
-    // Счетчик количества нажатий на элементы списка фильтров
-    private int selectionCount = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         FragmentQuestsBinding binding = FragmentQuestsBinding.inflate(getLayoutInflater());
 
-        Spinner filterSpinner = binding.filterSpinner;
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                requireContext(),
-                R.array.filterItems,
-                R.layout.view_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        filterSpinner.setAdapter(adapter);
-
-        // Выбор фильтра
-        filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i) {
-                    case 0:
-                        FilterListDialog listDialog = new FilterListDialog();
-                        listDialog.show(getActivity().getSupportFragmentManager(), "Filter lists dialog");
-                        break;
-                    case 1:
-                        if (selectionCount > 0) {
-                            FilterColorDialog colorDialog = new FilterColorDialog();
-                            colorDialog.show(getActivity().getSupportFragmentManager(), "Filter color dialog");
-                        } else selectionCount++;
-                        break;
-
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
+        // Фильтр задач
+        PowerSpinnerView filterSpinner = binding.filterSpinner;
+        filterSpinner.setOnSpinnerItemSelectedListener(
+                (OnSpinnerItemSelectedListener<CharSequence>) (oldIndex, oldItem, newIndex, newItem) -> {
+            switch (newIndex) {
+                case 0:
+                    FilterListDialog listDialog = new FilterListDialog();
+                    listDialog.show(getActivity().getSupportFragmentManager(), "Filter lists dialog");
+                    break;
+                case 1:
+                    FilterColorDialog colorDialog = new FilterColorDialog();
+                    colorDialog.show(getActivity().getSupportFragmentManager(), "Filter color dialog");
+                    break;
             }
         });
 
