dfc3ac7e86eb7d803bc787219ee92eaec7579150
Igor Streshnev <igor.streshnev2019@gmail.com>
2023-04-12 16:03:13 +0300

    fabric for registration vm

--- app/src/main/java/ru/mirea/playedu/viewmodel/RegistrationViewModelFabric.java
+++ app/src/main/java/ru/mirea/playedu/viewmodel/RegistrationViewModelFabric.java
@@ -1,4 +1,25 @@
 package ru.mirea.playedu.viewmodel;
 
-public class RegistrationViewModelFabric {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import ru.mirea.playedu.data.repository.UserRepository;
+import ru.mirea.playedu.data.repository.UserStatsRepository;
+
+public class RegistrationViewModelFabric implements ViewModelProvider.Factory {
+
+    private UserRepository userRepository;
+    private UserStatsRepository userStatsRepository;
+
+    public RegistrationViewModelFabric(UserRepository userRepository, UserStatsRepository userStatsRepository) {
+        this.userRepository = userRepository;
+        this.userStatsRepository = userStatsRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return ViewModelProvider.Factory.super.create(modelClass);
+    }
 }
